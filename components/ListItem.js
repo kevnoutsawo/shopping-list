14efebc46866de37c778afe388d360e99451f5c5
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-02-13 22:56:52 +0100

    Implemented delete function

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-Icon.loadFont();
-
-const ListItem = ({item}) => {
+const ListItem = ({item, deleteItem}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
           <Text style={styles.listItemText}>{item.text}</Text>
-          <Icon name="remove" size={20} color="firebrick" />
+          <Icon
+            name="remove"
+            size={20}
+            color="firebrick"
+            onPress={() => deleteItem(item.id)}
+          />
       </View>
     </TouchableOpacity>
   );
