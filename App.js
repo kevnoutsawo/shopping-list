661f42b6cf0a38bbf133475aa13908c6b5edb92f
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-02-13 18:38:04 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import { v4 as uuid } from 'uuid';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+})
+
+export default App;
